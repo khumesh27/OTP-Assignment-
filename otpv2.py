59b25b4a3bc02bbcd83faa8ed86af6bff140db9f
khumesh27 <125781605+khumesh27@users.noreply.github.com>
2023-10-19 01:12:09 +0530

    Update otpv2.py

--- otpv2.py
+++ otpv2.py
@@ -1,32 +1,83 @@
+import twilio
+from twilio.rest import Client
 import random
+import sms
 import smtplib
 import re
 
-def generate_OTP():
+client = Client(sms.account_sid, sms.Auth_token)
+account_sid='ACa5ce277d7bf4c0c517e5aed7ed70cc45'
+Auth_token='8a7ae3c8fbd04b006e83f4f503d7b75f'
+
+twilio_no="+17087940251"
+target_no="+99607465382"
+
+def generate_OTP():                                   # generate OTP
     global otp
     otp= ''.join([str(random.randint(0, 9)) for i in range(6)])
 
-def send_otp():
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login('khumeshkhobragade32119@gmail.com', 'qzlxjfftbfieqwmu')
-    msg = 'Dear Aspirant'+('Your Otp for login is /n') + str(otp)
-    server.sendmail('khumeshkhobragade32119@gmail.com', s, msg)
-    server.quit()
+fotp=generate_OTP()
 
-def validiate_email(s):
+def validate_mobile(num):                             # validating mobile numbebr
+    return len(num) == 10 and num.isdigit()
 
-  pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
-  if re.match(pat, s):
-        print('valid email')
-  else:
-        print('Invalid email Format, Please enter valid email address!!')
+def send_otp_over_mobile(target_no):                  #send OTP On SMS
+    if (validate_mobile(target_no)):
+        target_no = "+91" + target_no
+        message = client.messages.create(
+            body="\nYour OTP is "+str(otp),
+            from_=sms.twilio_no,
+            to=sms.target_no
+        )
+        print(message.body)
 
-global s
-s= input('Enter your email address\n')
+    else:
+        print("INVALID MOBILE NUMBER!")
+        target_no = input("ENTER AGAIN: ")
+        validate_mobile(target_no)
+        send_otp_over_mobile(target_no)
 
 
 
-print(generate_OTP())
-print(validiate_email(s))
-send_otp()
+def validate_email(mail):                              # validiate email id with Natural langauge Processing
+    pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+    if re.match(pat,mail):
+        return True;
+    else:
+        return False;
+
+
+
+def send_otp(mail):                                     #send OTP via Email
+    if (validate_email(mail)):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login('khumeshkhobragade32119@gmail.com', 'bvky byyh fnyc vumf')
+        msg = 'HELLO YOUR OTP IS ' + str(otp)
+        server.sendmail('khumeshkhobragade32119@gmail.com', mail, msg)
+        server.quit()
+        print("OTP sent! ")
+        print("Your OTP is",str(otp))
+    else:
+        print("INVALID MAIL!")
+        mail = input("ENTER AGAIN: ")
+        validate_email(mail)
+        send_otp(mail)
+
+
+#__Menu__
+
+print("\n========================================Welcome to generate OTP========================================")
+print("\n                                   <------CHOOSE PLATFORM------>                              \n\n1.SMS:\n2.Email:")
+ans = input("\nENTER YOUR CHOICE:\n")
+
+if (ans.lower() == "1"):
+
+    number = input("ENTER YOUR MOBILE NUMBER: ")
+    validate_mobile(number)
+    send_otp_over_mobile(number)
+
+elif (ans.lower() == "2"):
+
+    recipent = input("ENTER YOUR MAIL ID:\n ")
+    send_otp(recipent)
